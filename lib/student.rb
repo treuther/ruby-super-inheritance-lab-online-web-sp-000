4e2b2407f906269e15d7b4d6a664034fa9328721
Tracy Reuther <tracyreuther@gmail.com>
2019-07-08 22:30:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,5 @@
 class Student   #parent (super) class
 
-  attr_accessor :student
-
-  def initialize(student)
-    @student = student
-  end
-
   def hello
     "Hello there! I'm so excited to learn stuff."
   end
